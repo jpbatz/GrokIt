1bfa8e84d3b6241444e56317ce687438fd548d79
jpbatz <jmacnut@gmail.com>
2015-01-13 03:28:03 +0000

    c9

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@
 app.set('view engine', 'jade');
 // to specify other than views folder:
 // __dirname is global var for current dir path where app.js is located
-// ap.set('views', __dirname + '/partials');
+// ap.set('views', __dirname + '/includes');
 
  app.get('/', function(req, res) {
   // res.send('<h1>Hello</h1> Express');
